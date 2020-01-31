ab3831e7aa4e733e9f818b1edbe5b25ecfce570f
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-31 18:05:12 +0300

    Config

--- ConfigurationSample3/Default.aspx.cs
+++ ConfigurationSample3/Default.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +11,25 @@ namespace ConfigurationSample3
 {
     public partial class Default : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void ReadButton_Click(object sender, EventArgs e)
         {
-
+            string path = @"system.web/customerErrors";
+            CustomErrorsSection section = WebConfigurationManager.GetSection(path) as CustomErrorsSection;
+            foreach(CustomError item in section.Errors)
+            {
+                Label1.Text += item.StatusCode;
+                Label1.Text += " ";
+                Label1.Text += item.Redirect;
+                Label1.Text += "<br/>";
+            }
+        }
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            Configuration cfg = WebConfigurationManager.OpenWebConfiguration("/");
+            CustomErrorsSection section = cfg.GetSection("system.web/customErrors") as CustomErrorsSection;
+            CustomError tempErorr = new CustomError(501, "~/not_implemented.aspx");
+            section.Errors.Add(tempErorr);
+            cfg.Save();
         }
     }
 }
